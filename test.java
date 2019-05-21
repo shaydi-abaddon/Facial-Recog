7069e3df25c7283c882f34882fbf1404e2084f01
cheseremtitus24 <50730372+cheseremtitus24@users.noreply.github.com>
2019-05-21 12:56:46 +0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double xvalues[] = {0,8,12,20};
+		MethodCovarianceCalc calc = new MethodCovarianceCalc( );
+		  calc.findmean(xvalues);
+		  
+		
+
+	}
+
+}
